7b752a5c6e5cfb333eb5c5715534b3bda21443e4
shreyakhandelwal777 <khandelwalshreya83@gmail.com>
2019-05-30 14:36:17 +0530

    first shreya

---
+++ precache-manifest.f16e072c6ef042da79ef93a7454a7587.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2084f38f3d12cb94488234f1dff4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be27b59fe8ad3fc85e61",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "163a54f29a9dacf09102",
+    "url": "/static/css/main.0b27fa84.chunk.css"
+  },
+  {
+    "revision": "be27b59fe8ad3fc85e61",
+    "url": "/static/js/2.fea0ce0e.chunk.js"
+  },
+  {
+    "revision": "163a54f29a9dacf09102",
+    "url": "/static/js/main.33b31c2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6d05c21bc2845143b720df72fe0a37f",
+    "url": "/static/media/ebazar.b6d05c21.jpeg"
+  }
+]);
